47893de9557c20869fa180700573572790b3ce77
skz <keerthana6568@gmail.com>
2025-07-03 19:29:01 +0530

    Day 1 & 2 ✅

---
+++ day2/main.py
@@ -0,0 +1,10 @@
+
+
+print("DAY -2 of 100 days of code")
+#input("Name: ") #variable = to store the inputs we give 
+x= input("what's your name : ")
+y=input("what's your fav food? ")
+z=input("where do you live? ")
+print("name is :", x)
+print(f" fav food is {y}")
+print(z)
