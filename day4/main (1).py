2a4e7396d7c393bf3e81186fa7f5c1687431c97a
skz <keerthana6568@gmail.com>
2025-07-05 21:10:16 +0530

    Day 4 ✅

---
+++ day4/main (1).py
@@ -0,0 +1,14 @@
+#DAY 4 (print in color)
+
+#print("Uh, oh, you've been given a", "\033[31m", "warning", "\033[0m", "for being a bad, bad person.") 
+
+print("-----A STORY-----")
+print("---give the respone to the following code---")
+x = input("Enter a name : ")
+y = input("Enter a your fav song : ")
+z = input("Enter a your fav book : ")
+print()
+print("User name is","\033[34m", x ,"\033[0m")
+print("User fav song is","\033[35m", y,"\033[0m")
+print("User fav book is","\033[36m", z)
+
