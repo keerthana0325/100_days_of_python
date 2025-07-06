55ac8f7fb2c566325dc30592fa566a3b0eff70fc
skz <keerthana6568@gmail.com>
2025-07-06 21:03:33 +0530

    day5 ✅

---
+++ day5/day5.py
@@ -0,0 +1,18 @@
+#DAY 5 decision making (if statment)
+#✏️  input() always gives a string.
+#🧪  Convert it for math.
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+if(a>b):
+  c = a+b
+  print("First condition - ")
+  print("The sum is: ", c)
+else: 
+    c = a-b
+    print("Second conition - ")
+    print("The difference is: ", c)
+
+  
+
